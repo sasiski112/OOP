346d00dd0f4668a933d8056bdedea7e8e881fe56
sasiski112 <mshevelovv@gmail.com>
2026-04-15 15:26:43 +0300

    Update Õpetaja.cs

--- Õpetaja.cs
+++ Õpetaja.cs
@@ -1,12 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace OOP
 {
-    public class Õpetaja : Isik
+    public class Õpetaja : Isik, ITööline
     {
         public string Aine { get; set; }
+        public double Tunnitasu { get; set; } = 8.3;
+        public double Tunidkuus { get; set; } = 88;
+        public TööTüüp VäljamakseTüüp { get; set; } = TööTüüp.Palk;
+
 
         public void Õpeta()
         {
@@ -15,6 +19,10 @@ namespace OOP
         public override void Kirjelda()
         {
             Console.WriteLine($"Mina olen õpetaja {Nimi} ja ma õpetan: {Aine}.");
+        }
+        public double ArvutPalk()
+        {
+            return Tunnitasu * Tunidkuus;
         }
     }
 }
