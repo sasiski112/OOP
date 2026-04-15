11d9620c982edcc13be4bf1a0e42463071b47fbd
sasiski112 <mshevelovv@gmail.com>
2026-04-15 15:25:05 +0300

    Update Isik.cs

--- Isik.cs
+++ Isik.cs
@@ -1,11 +1,12 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace OOP
 {
-    public class Isik
+    public abstract class Isik
     {
+        public abstract void Kirjelda();
 
         private int sünniaasta;
         public string Nimi { get; set; }
@@ -15,27 +16,55 @@ namespace OOP
             get { return sünniaasta; }
             set
             {
-                if (Sünniaasta > 1900 && Sünniaasta <= DateTime.Now.Year)
+                if (value > 1900 && value <= DateTime.Now.Year)
                     sünniaasta = value;
                 else
                     Console.WriteLine("Vigane sünniaasta!");
             }
         }
+
         public int Vanus => sünniaasta == 0 ? 0 : DateTime.Now.Year - sünniaasta;
+
         public void Tervita()
         {
             if (string.IsNullOrEmpty(Nimi) || sünniaasta == 0)
-            {
                 Console.WriteLine("vale");
-            }
             else
-            {
                 Console.WriteLine($"Tere! Mina olen {Nimi} ja ma olen {Vanus} aastat vana. Ma olen {Sünniaasta}");
-            }
-
         }
-        // Abstraktne meetod – sisu puudub, alamklassid PEAVAD selle ise looma
-        public abstract void Kirjelda();
+    }
+    public class Koolihaldus
+    {
+        // Kapseldatud list
+        private List<Isik> inimesed = new List<Isik>();
+
+        public void LisaInimene(Isik isik)
+        {
+            inimesed.Add(isik);
+        }
+
+        public void KuvaKõik()
+        {
+            Console.WriteLine("\n--- KOOLI NIMEKIRI ---");
+            foreach (var isik in inimesed)
+            {
+                // Polümorfism teeb siin imesid! 
+                // C# teab ise, kas käivitada Õpetaja või Õpilase Kirjelda() meetod.
+                isik.Kirjelda();
+            }
+        }
+        public static void Main()
+        {
+            Koolihaldus minuKool = new Koolihaldus();
+
+            Õpetaja op = new Õpetaja { Nimi = "Mati", Aine = "Programmeerimine" };
+            Õpilane opilane1 = new Õpilane { Nimi = "Mari", Klass = 10, Staatus = Õppevorm.Päevane };
+
+            minuKool.LisaInimene(op);
+            minuKool.LisaInimene(opilane1);
+
+            minuKool.KuvaKõik();
+        }
     }
 }
 
